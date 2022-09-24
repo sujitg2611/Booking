0afd986c9fdcd096fbb0482e7f67745042a9eefe
jitu712 <jitu712@gmail.com>
2022-09-24 11:48:16 +0530

    first commit

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,50 @@
+import "./featuredProperties.css"
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+        <div className="fpItem">
+            <img src="" alt="" className="fpImg" />
+            <span className="fpName">Taj Hotel</span>
+            <span className="fpCity">Mumbai</span>
+            <span className="fpPrice">Starting from Rs10000</span>
+            <div className="fpRating">
+                <button>9.4</button>
+                <span>Excellent</span>
+            </div>
+        </div>
+        <div className="fpItem">
+        <img src="" alt="" className="fpImg" />
+        <span className="fpName">Sunrise Hotel</span>
+        <span className="fpCity">Mumbai</span>
+        <span className="fpPrice">Starting from Rs10000</span>
+        <div className="fpRating">
+            <button>9.4</button>
+            <span>Excellent</span>
+        </div>
+        </div>
+        <div className="fpItem">
+        <img src="" alt="" className="fpImg" />
+        <span className="fpName">Taj Hotel</span>
+        <span className="fpCity">Mumbai</span>
+        <span className="fpPrice">Starting from Rs10000</span>
+        <div className="fpRating">
+            <button>9.4</button>
+            <span>Excellent</span>
+        </div>
+        </div>
+        <div className="fpItem">
+        <img src="" alt="" className="fpImg" />
+        <span className="fpName">Sunrise Hotel</span>
+        <span className="fpCity">Mumbai</span>
+        <span className="fpPrice">Starting from Rs10000</span>
+        <div className="fpRating">
+            <button>9.4</button>
+            <span>Excellent</span>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedProperties
---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,45 @@
+import "./propertyList.css"
+
+const propertyList = () => {
+  return (
+    <div className="pList">
+        <div className="pListItem">
+            <img src="" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Hotels</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+        <div className="pListItem">
+            <img src="" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Apartment</h1>
+                <h2>233 apartment</h2>
+            </div>
+        </div>
+        <div className="pListItem">
+            <img src="" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Resort</h1>
+                <h2>233 resort</h2>
+            </div>
+        </div>
+        <div className="pListItem">
+            <img src="" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Villas</h1>
+                <h2>233 villas</h2>
+            </div>
+        </div>
+        <div className="pListItem">
+            <img src="" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Cabins</h1>
+                <h2>233 cabins</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default propertyList
