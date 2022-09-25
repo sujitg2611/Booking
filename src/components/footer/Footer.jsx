119db071366373543ceffafc7ed078af1bab49ab
jitu712 <jitu712@gmail.com>
2022-09-25 09:45:50 +0530

    added a footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import "./footer.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="fLists">
+        <ul className="fList">
+          <li className="firstListItem">States</li>
+          <li className="firstListItem">Regions</li>
+          <li className="firstListItem">Cities</li>
+          <li className="firstListItem">Districts</li>
+          <li className="firstListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="firstListItem">States</li>
+          <li className="firstListItem">Regions</li>
+          <li className="firstListItem">Cities</li>
+          <li className="firstListItem">Districts</li>
+          <li className="firstListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="firstListItem">States</li>
+          <li className="firstListItem">Regions</li>
+          <li className="firstListItem">Cities</li>
+          <li className="firstListItem">Districts</li>
+          <li className="firstListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="firstListItem">States</li>
+          <li className="firstListItem">Regions</li>
+          <li className="firstListItem">Cities</li>
+          <li className="firstListItem">Districts</li>
+          <li className="firstListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="firstListItem">States</li>
+          <li className="firstListItem">Regions</li>
+          <li className="firstListItem">Cities</li>
+          <li className="firstListItem">Districts</li>
+          <li className="firstListItem">Hotels</li>
+        </ul>
+      </div>
+      <div className="fText">Copyright @ 2022 Booking</div>
+    </div>
   )
 }
 
