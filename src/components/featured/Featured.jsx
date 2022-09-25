1650ccb3118e2b4f0aec306974e450eee4ca33c8
jitu712 <jitu712@gmail.com>
2022-09-25 10:32:14 +0530

    Added Images in the Website

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,24 +1,27 @@
 import "./featured.css";
+import mumbaiPic from "../../Images/Mumbai.jpg"
+import bangalorePic from "../../Images/Bangalore.jpg"
+import goaPic from "../../Images/Goa.jpg"
 
 const Featured = () => {
   return (
     <div className="featured">
         <div className="featuredItem">
-            <img src="https://t-cf.bstatic.com/xdata/images/city/square250/971346.webp?k=40eeb583a755f2835f4dcb6900cdeba2a46dc9d50e64f2aa04206f5f6fce5671&o=" alt="" className="featuredImg" />
+            <img src={mumbaiPic} alt="" className="featuredImg" />
             <div className="featuredTitle">
                 <h1>Mumbai</h1>
                 <h2>123 properties</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img src="https://t-cf.bstatic.com/xdata/images/region/square250/68606.webp?k=4b43b9128b79beaab4ca2e8c038ddf5709b0b90608afbca222cfbe08fabda7d2&o=" alt="" className="featuredImg" />
+            <img src={goaPic} alt="" className="featuredImg" />
             <div className="featuredTitle">
                 <h1>Goa</h1>
                 <h2>223 properties</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img src="https://t-cf.bstatic.com/xdata/images/city/square250/684534.webp?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o=" alt="" className="featuredImg" />
+            <img src={bangalorePic} alt="" className="featuredImg" />
             <div className="featuredTitle">
                 <h1>Bangalore</h1>
                 <h2>323 properties</h2>
--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -1,41 +1,46 @@
 import "./propertyList.css"
+import star5Hotel from "../../Images/Hotel.jpg"
+import star4Hotel from "../../Images/4starHotel.jpg"
+import star3Hotel from "../../Images/3starHotel.jpg"
+import resortHotel from "../../Images/ResortHotel.jpg"
+import Lodge from "../../Images/Lodge.jpg"
 
 const propertyList = () => {
   return (
     <div className="pList">
         <div className="pListItem">
-            <img src="" alt="" className="pListImg" />
+            <img src={star5Hotel} alt="" className="pListImg" />
             <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233 hotels</h2>
+                <h1>5 Star Hotels</h1>
+                <h2>200 Hotels</h2>
             </div>
         </div>
         <div className="pListItem">
-            <img src="" alt="" className="pListImg" />
+            <img src={star4Hotel} alt="" className="pListImg" />
             <div className="pListTitles">
-                <h1>Apartment</h1>
-                <h2>233 apartment</h2>
+                <h1>4 Star Hotels</h1>
+                <h2>233 Hotels</h2>
             </div>
         </div>
         <div className="pListItem">
-            <img src="" alt="" className="pListImg" />
+            <img src={star3Hotel} alt="" className="pListImg" />
             <div className="pListTitles">
-                <h1>Resort</h1>
-                <h2>233 resort</h2>
+                <h1>3 Star Hotels</h1>
+                <h2>300 Hotels</h2>
             </div>
         </div>
         <div className="pListItem">
-            <img src="" alt="" className="pListImg" />
+            <img src={resortHotel} alt="" className="pListImg" />
             <div className="pListTitles">
-                <h1>Villas</h1>
-                <h2>233 villas</h2>
+                <h1>Resort Hotels</h1>
+                <h2>400 Resort Hotels</h2>
             </div>
         </div>
         <div className="pListItem">
-            <img src="" alt="" className="pListImg" />
+            <img src={Lodge} alt="" className="pListImg" />
             <div className="pListTitles">
-                <h1>Cabins</h1>
-                <h2>233 cabins</h2>
+                <h1>Lodge</h1>
+                <h2>500 Lodges</h2>
             </div>
         </div>
     </div>
