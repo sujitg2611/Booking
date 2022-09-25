0ed8fba6d792e9477f4849fa8e7a579a433828fd
jitu712 <jitu712@gmail.com>
2022-09-25 09:46:54 +0530

    updated images in featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,23 +4,23 @@ const Featured = () => {
   return (
     <div className="featured">
         <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" alt="" className="featuredImg" />
+            <img src="https://t-cf.bstatic.com/xdata/images/city/square250/971346.webp?k=40eeb583a755f2835f4dcb6900cdeba2a46dc9d50e64f2aa04206f5f6fce5671&o=" alt="" className="featuredImg" />
             <div className="featuredTitle">
-                <h1>Dublin</h1>
+                <h1>Mumbai</h1>
                 <h2>123 properties</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img src="" alt="" className="featuredImg" />
+            <img src="https://t-cf.bstatic.com/xdata/images/region/square250/68606.webp?k=4b43b9128b79beaab4ca2e8c038ddf5709b0b90608afbca222cfbe08fabda7d2&o=" alt="" className="featuredImg" />
             <div className="featuredTitle">
-                <h1>Austin</h1>
+                <h1>Goa</h1>
                 <h2>223 properties</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img src="" alt="" className="featuredImg" />
+            <img src="https://t-cf.bstatic.com/xdata/images/city/square250/684534.webp?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o=" alt="" className="featuredImg" />
             <div className="featuredTitle">
-                <h1>Reno</h1>
+                <h1>Bangalore</h1>
                 <h2>323 properties</h2>
             </div>
         </div>
